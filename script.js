268ce2607997ba542647c6e70c79afaac04cd290
jewelvaida <jewelvaidaajaime@gmail.com>
2023-09-24 13:21:41 +0800

    Updated index.html, script.js, and style.css
    Hamburger Nav.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Hamburger Navigation
+function toggleMenu() {
+  const menu = document.querySelector(".hamburger-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
 // About Section
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
