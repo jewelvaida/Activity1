a7c3c3c8e4bb8e3ae26f3e7db8d806090025a03b
jewelvaida <jewelvaidaajaime@gmail.com>
2023-09-24 20:04:17 +0800

    Updated script.js
    Fixed the swiper in the project section.

--- script.js
+++ script.js
@@ -26,8 +26,9 @@ const swiper = new Swiper(".swiper", {
   direction: "vertical",
   loop: true,
 
-  pagination: {
-    el: ".swiper-pagination",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
